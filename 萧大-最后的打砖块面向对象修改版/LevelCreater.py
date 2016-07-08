4d9fc8d924c452fcb97b2c0db90a5e8c8126ea03
Tokaya <liaosl1234@gmail.com>
2016-07-08 19:34:09 +0800

    Revert "07/08/16更新"
    This reverts commit 38befe0ad0c849f47fbd2554f8c6ac1a5ffc1fd8.

--- 萧大-最后的打砖块面向对象修改版/LevelCreater.py
+++ 萧大-最后的打砖块面向对象修改版/LevelCreater.py
@@ -4,10 +4,7 @@ from cocos.layer import Layer
 from cocos.text import Label
 from cocos.sprite import Sprite
 from pyglet.window.key import symbol_string
-from cocos.menu import *
-from cocos.scene import *
-from cocos.scenes import SplitColsTransition
-import game
+import json
 
 
 class LevelCreater(Layer):
@@ -17,12 +14,13 @@ class LevelCreater(Layer):
 		super(LevelCreater, self).__init__()
 		label = Label("关卡编辑器, 按空格保存关卡文件")
 		self.add(label)
-		self.list = []
+		self.list = ['1']
 		self.zhuan = Sprite('images/zhuan.png')
 		self.add(self.zhuan)
 
 	def on_mouse_motion(self, x, y, dx, dy):
 		self.zhuan.position = (x, y)
+		# print(x, y, dx, dy)
 
 	def on_mouse_press(self, x, y, buttons, modifiers):
 		x1 = x // 35 * 35
@@ -52,44 +50,8 @@ class LevelCreater(Layer):
 				f.write(level)
 
 
-class Back(Menu):
-	def __init__(self, back):
-		super(Back, self).__init__()
-		self.back = back
-		self.menu_valign = BOTTOM
-		self.menu_halign = RIGHT
-		self.font_item = {
-			'font_name': 'ALISON',
-			'font_size': 32,
-			'bold': False,
-			'italic': False,
-			# 'anchor_y': 'center',
-			# 'anchor_x': 'center',
-			'color': (192, 192, 192, 255),
-			'dpi': 96,
-		}
-		self.font_item_selected = {
-			'font_name': 'ALISON',
-			'font_size': 42,
-			'bold': False,
-			'italic': False,
-			# 'anchor_y': 'center',
-			# 'anchor_x': 'center',
-			'color': (255, 255, 255, 255),
-			'dpi': 96,
-		}
-		items = []
-		items.append(MenuItem('Back', self.back))
-		self.create_menu(items, zoom_in(), zoom_out())
-
-	def back(self):
-		scene = Scene(game.Title())
-		director.replace(SplitColsTransition(scene))
-
-
 if __name__ == '__main__':
 	director.init()
-	hello_layer = LevelCreater()
-	main_scene = Scene(hello_layer)
-	main_scene.add(Back(hello_layer))
-	director.run(main_scene)
+	main_layer = LevelCreater()
+	scene = scene.Scene(main_layer)
+	director.run(scene)
